a569c1470a2f2eeea9e95aaf8de602c0b12cd507
Nicolas-62 <nlourdel62000@gmail.com>
2020-01-23 19:56:04 +0100

    test config 2

--- bin/common.sh
+++ bin/common.sh
@@ -104,7 +104,7 @@ https://devcenter.heroku.com/articles/scala-support"
 install_play()
 {
   VER_TO_INSTALL=$1
-  PLAY_URL="https://s3.amazonaws.com/heroku-jvm-langpack-play/play-heroku-$VER_TO_INSTALL.tar.gz"
+  PLAY_URL="https://drive.google.com/open?id=1guzGkYN0CGZTXiMIv1W-7mStQ9cQn-y8"
   PLAY_TAR_FILE="play-heroku.tar.gz"
 
   validate_play_version ${VER_TO_INSTALL}
